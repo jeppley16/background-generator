c053d9a2e6d02b8d8c5692e5f57b0c79bebf7e74
jeppley16 <justin.eppley@gmail.com>
2018-06-09 16:45:10 -0400

    Revised initial files

--- script.js
+++ script.js
@@ -2,47 +2,18 @@ var css = document.querySelector("h3");
 var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2");
 var body = document.getElementById("gradient");
-var button = document.querySelector("button");
 
-// console.log(css);
-// console.log(color1);
-// console.log(color2);
-
-function bgstyler() {
+function setGradient() {
 	body.style.background = 
 	"linear-gradient(to right, " 
 	+ color1.value 
 	+ ", " 
 	+ color2.value 
-	+ ")";	
+	+ ")";
 
 	css.textContent = body.style.background + ";";
 }
 
-function setGradientColor() {
-	body.style.background = 
-	"linear-gradient(to right, " 
-	+ color1.value 
-	+ ", " 
-	+ color2.value 
-	+ ")";	
+color1.addEventListener("input", setGradient);
 
-	css.textContent = body.style.background + ";";
-}	
-
-button.addEventListener("click", function() {
-	var randNumber = Math.floor((Math.random() * 256));
-	return randNumber.toString(16);
-	var newColor = "#";
-})
-
-
-
-setGradientColor();
-color1.addEventListener("input", bgstyler);
-color2.addEventListener("input", bgstyler);
-
-
-
-
-
+color2.addEventListener("input", setGradient);
